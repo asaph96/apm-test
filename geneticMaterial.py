504fd7d6d24d7840111abf0456262dfbdadb8fe9
asaph96 <asaph@ufu.br>
2019-04-11 06:39:59 -0300

    Add gene name option

--- geneticMaterial.py
+++ geneticMaterial.py
@@ -1,11 +1,16 @@
 class DNA(object):
 
-    def __init__(self, alelos: list):
+    def __init__(self, alelos: list, gene_names: list = None):
         self.__alelos = alelos
+        self.__genname = gene_names
 
     @property
     def alelos(self) -> dict:
         alelos = dict()
-        for gene in self.__alelos:
-            alelos[f"gene{self.__alelos.index(gene)+1}"] = gene
+        if self.__genname == None:
+            for gene in self.__alelos:
+                alelos[f"gene{self.__alelos.index(gene)+1}"] = gene
+        else:
+            for gene, name in zip(self.__alelos, self.__genname):
+                alelos[f"{name}"] = gene
         return alelos
